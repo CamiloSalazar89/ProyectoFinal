4ea435ae064ca6f13691f688d7024326eae9c26e
CamiloSalazar89 <117320345+CamiloSalazar89@users.noreply.github.com>
2022-11-16 23:43:08 -0300

    Actualizacion para entrega1SalazarFuentes

--- Libreria/urls.py
+++ Libreria/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('inicio', views.inicio, name='inicio'),
-    path( 'home', views.home, name='home'),
     path('producto', views.producto, name='producto'),
     path('blog', views.blog, name='blog'),
     path('contacto', views.contacto, name='contacto'),
--- Libreria/views.py
+++ Libreria/views.py
@@ -6,9 +6,6 @@ from django.http import HttpResponse
 def inicio (request):
     return render(request, 'paginas/Inicio.html')
 
-def home (request):
-    return render(request, 'paginas/Home.html')
-
 def producto(request):
     return render(request, 'producto/index.html')
 
