9eb96f4417e859030988b2e6d4b9d5b81ed3feca
CamiloSalazar89 <117320345+CamiloSalazar89@users.noreply.github.com>
2022-11-17 00:23:59 -0300

    Actualizacion

--- Libreria/admin.py
+++ Libreria/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import producto
+from .models import Juguetes
+from .models import Alimentacion
+from .models import Accesorios
 
-admin.site.register(producto)
+admin.site.register(Juguetes)
+admin.site.register(Accesorios)
+admin.site.register(Alimentacion)
+
+
 # Register your models here.
--- Libreria/models.py
+++ Libreria/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class producto(models.Model):
+class Juguetes (models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, verbose_name= 'Nombre')
     imagen = models.ImageField(upload_to='Imagenes/',verbose_name='Imagen', null= True)
@@ -16,7 +16,35 @@ class producto(models.Model):
     def delete(self, using=None, keep_parents=False):
         self.imagen.storage.delete(self.imagen.name)
         super().delete()
-        
+
+class Alimentacion (models.Model):
+    id = models.AutoField(primary_key=True)
+    tipo = models.CharField(max_length=100, verbose_name= 'Tipo de animal')
+    imagen = models.ImageField(upload_to='Imagenes/',verbose_name='Imagen', null= True)
+    descripcion= models.TextField(verbose_name= 'Descripción', null=True)
+    categoria = models.CharField(verbose_name= 'Categoria', max_length=100, null=True)
+
+    def __str__(self):
+        fila = 'Tipo de animal' + self.tipo + '-' + 'Descripcion:' + self.descripcion + 'Categoria:' + self.categoria
+        return fila
+
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
+
+class Accesorios (models.Model):
+    id = models.AutoField(primary_key=True)
+    tipo = models.CharField(max_length=100, verbose_name= 'Tipo de animal')
+    imagen = models.ImageField(upload_to='Imagenes/',verbose_name='Imagen', null= True)
+    descripcion= models.TextField(verbose_name= 'Descripción', null=True)
+    categoria = models.CharField(verbose_name= 'Categoria', max_length=100, null=True)
 
 
 
+    def __str__(self):
+        fila = 'Tipo de animal' + self.tipo + '-' + 'Descripcion:' + self.descripcion + 'Categoria:' + self.categoria
+        return fila
+
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
