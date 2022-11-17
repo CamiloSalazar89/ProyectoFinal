61a07951b96f4215680fa5d63d3f8e0d0af7167f
CamiloSalazar89 <117320345+CamiloSalazar89@users.noreply.github.com>
2022-11-17 00:41:52 -0300

    Actualizacion

--- Libreria/views.py
+++ Libreria/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Juguetes
+from .models import Alimentacion
+from .models import Accesorios
 
 # Create your views here.
 
@@ -7,7 +10,11 @@ def inicio (request):
     return render(request, 'paginas/Inicio.html')
 
 def producto(request):
-    return render(request, 'producto/index.html')
+    Juguetes = Juguetes.objects.all()
+    Alimentacion = Alimentacion.objects.all()
+    Accesorios = Accesorios.objects.all()
+   
+    return render(request, 'producto/index.html', {'Juguetes': Juguetes , 'Alimentacion': Alimentacion  , 'Accesorios': Accesorios})
 
 def blog (request):
     return render(request, 'paginas/blog.html')
